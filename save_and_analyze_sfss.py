2bfada5d5a8d9faaa5736162eaf27c5d50566382
David Bass <davidbass256@gmail.com>
2022-11-18 23:06:16 -0500

    new sfs stats addition

--- save_and_analyze_sfss.py
+++ save_and_analyze_sfss.py
@@ -6,14 +6,18 @@ from math import log
 
 # Setup
 dir = "/scratch/djb3ve/connor/"
-pickle_file = dir + "run_moments/daphnia_filtered_data_dict.pickle"
+#pickle_file = dir + "run_moments/daphnia.filtered.chr.busco_data_dict.pickle"
+pickle_file = dir + "run_moments/daphnia.filt.mlg.genome.11.18.22_data_dict.pickle"
 output_file = dir + "sfs_statistics.txt"
 csv_dir = dir + "sfs_csvs/"
 sfs_dir = dir + "sfss/"
 pop_ids = ["Daphnia.pulex.NorthAmerica", "Daphnia.pulex.Europe"]
 prior_estimated_params = [7e5, 2e5, 1e7 / 2, 1e-8 * 2]
-split_mig_params = [6.358476, 1.032427, 9.989897, 0.007727318]
-split_no_mig_params = [1.065988, 0.2213014, 0.8256301, 0]
+#split_mig_params = [6.358476, 1.032427, 9.989897, 0.007727318]
+#split_no_mig_params = [1.065988, 0.2213014, 0.8256301, 0]
+split_mig_params = [6.758185, 1.1126711, 9.9724427, 0.008836859]
+split_no_mig_params = [1.127693, 0.2289919, 0.7780228, 0]
+
 
 def save_sfs_as_csv_and_npy(sfs, sfs_name):
     # Save SFS as CSV
@@ -72,22 +76,22 @@ with open(pickle_file, "rb") as f:
     data_dict = pickle.load(f)
 
 # ns gives dimensions of SFS as [ns[0] + 1, ns[1] + 1]
-with open(output_file, "w") as fout:
+with open(output_file, "a") as fout:
     # Write header
-    fout.write("n\tsfs\tAIC\tBIC\tSAP\n")
+    #fout.write("n\tsfs\tAIC\tBIC\tSAP\n")
 
     for ns in [[20, 20], [100, 100]]:
         sfs_empirical = moments.Spectrum.from_data_dict(data_dict, pop_ids=pop_ids,
                                                   projections=ns,
                                                   polarized=False)
         save_sfs_as_csv_and_npy(sfs_empirical, "sfs_empirical_" + str(ns[0]))
-        outputs = [ns[0], "sfs_empirical", "NA", "NA", get_shared_allele_prop(sfs_empirical)]
+        outputs = ["mlg", ns[0], "sfs_empirical", "NA", "NA", get_shared_allele_prop(sfs_empirical)]
         write_output(fout, outputs)
 
         sfs_from_ests = moments.Demographics2D.split_mig(prior_estimated_params, ns,
                                                    pop_ids=pop_ids).fold()
         save_sfs_as_csv_and_npy(sfs_from_ests, "sfs_from_ests_" + str(ns[0]))
-        outputs = [ns[0], "sfs_from_ests",
+        outputs = ["mlg", ns[0], "sfs_from_ests",
                    get_AIC(sfs_from_ests, sfs_empirical, 4),
                    get_BIC(sfs_from_ests, sfs_empirical, 4, ns[0] + 1),
                    get_shared_allele_prop(sfs_from_ests)]
@@ -96,7 +100,7 @@ with open(output_file, "w") as fout:
         sfs_split_mig_model = moments.Demographics2D.split_mig(split_mig_params, ns,
                                                               pop_ids=pop_ids).fold()
         save_sfs_as_csv_and_npy(sfs_split_mig_model, "sfs_split_mig_model_" + str(ns[0]))
-        outputs = [ns[0], "sfs_split_mig_model",
+        outputs = ["mlg", ns[0], "sfs_split_mig_model",
                    get_AIC(sfs_split_mig_model, sfs_empirical, 4),
                    get_BIC(sfs_split_mig_model, sfs_empirical, 4, ns[0] + 1),
                    get_shared_allele_prop(sfs_split_mig_model)]
@@ -105,7 +109,7 @@ with open(output_file, "w") as fout:
         sfs_split_no_mig_model = moments.Demographics2D.split_mig(split_no_mig_params, ns,
                                                                  pop_ids=pop_ids).fold()
         save_sfs_as_csv_and_npy(sfs_split_no_mig_model, "sfs_split_no_mig_model_" + str(ns[0]))
-        outputs = [ns[0], "sfs_split_no_mig_model",
+        outputs = ["mlg", ns[0], "sfs_split_no_mig_model",
                    get_AIC(sfs_split_no_mig_model, sfs_empirical, 3),
                    get_BIC(sfs_split_no_mig_model, sfs_empirical, 3, ns[0] + 1),
                    get_shared_allele_prop(sfs_split_no_mig_model)]
